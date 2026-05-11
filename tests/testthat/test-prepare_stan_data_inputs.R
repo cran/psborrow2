24aee2d1bb5dbc727972c2d597d197aea3e1cda1
Matt Secrest <secrestm@gene.com>
2026-05-11 22:10:02 +0000

    version 0.0.5.0

--- tests/testthat/test-prepare_stan_data_inputs.R
+++ tests/testthat/test-prepare_stan_data_inputs.R
@@ -127,7 +127,6 @@ test_that("prepare_stan_data_inputs works with weights", {
 
 
 test_that("prepare_stan_data_inputs works with PEM", {
-  
   object <- psborrow2:::.analysis_obj(
     data_matrix = example_matrix,
     outcome = outcome_surv_pem(
@@ -148,5 +147,27 @@ test_that("prepare_stan_data_inputs works with PEM", {
   expect_equal(names(result), c("N", "trt", "time", "cens", "N_periods", "Z0", "Z1"))
   expect_equal(dim(result[["Z0"]])[2], result[["N_periods"]])
   expect_equal(dim(result[["Z1"]])[2], result[["N_periods"]])
+})
 
+
+test_that("prepare_stan_data_inputs works with a fixed power prior", {
+  object <- psborrow2:::.analysis_obj(
+    data_matrix = example_matrix,
+    outcome = outcome_surv_exponential(
+      "time",
+      "cnsr",
+      prior_normal(0, 1000)
+    ),
+    borrowing = borrowing_fixed_power_prior(
+      "ext",
+      0.8
+    ),
+    treatment = treatment_details("trt", prior_normal(0, 1000))
+  )
+
+  result <- psborrow2:::prepare_stan_data_inputs(object@outcome, object@borrowing, object)
+  power <- result$power
+
+  expect_atomic_vector(power)
+  expect_equal(sum(power == 1), sum(1 - example_matrix[, "ext"]))
 })
